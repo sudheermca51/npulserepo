52b41cb37ef645a56da9ce567cb6ffaecea2d4aa
sudheermca51 <37737996+sudheermca51@users.noreply.github.com>
2026-07-18 05:52:14 +0530

    Refactor username locator in LoginPage class

--- nexuspulse/src/main/java/org/iitwf/nexuspulse/admin/pages/LoginPage.java
+++ nexuspulse/src/main/java/org/iitwf/nexuspulse/admin/pages/LoginPage.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
 
 	WebDriver driver;
+	private By usernameLoc = By.name("username");
 	public LoginPage(WebDriver driver)
 	{
 		this.driver = driver;		
 	}
 	public void adminLogin(String uName,String pWord) {
 		 
-		driver.findElement(By.name("username")).sendKeys(uName);
+		driver.findElement(usernameLoc).sendKeys(uName);
 		driver.findElement(By.name("password")).sendKeys(pWord);
 		driver.findElement(By.xpath("//button[@class='btn-primary']")).click();
 	}
